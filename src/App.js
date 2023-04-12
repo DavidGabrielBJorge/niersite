a3da9196af371a72e7282b604d29f2c7ca67518c
DavidGabrielBJorge <david.jorge@estudante.iftm.edu.br>
2023-04-12 12:36:38 -0300

    add: implementando o react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";//Permite isar componentes do React Router
 import AboutUs from "./view/aboutUs";
 import MainPage from "./view/mainPage";
 
-console.log(window.location)
-
-const pagina = window.location.pathname === '/' ? <MainPage></MainPage> : <AboutUs></AboutUs>
 //casona url for / vai carregar a página de início, se não for vai ser a sobre
 
 function App() {
-  return pagina;
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<MainPage></MainPage>}/>
+          <Route path='/aboutus' element={<AboutUs></AboutUs>}/>
+          <Route path='*' element={<div>página não encontrada</div>}/>
+
+          
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
