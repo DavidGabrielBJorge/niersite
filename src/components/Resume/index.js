81eb3bf829b5d56de6ee615923a772f9c9adf2d8
DavidGabrielBJorge <david.jorge@estudante.iftm.edu.br>
2023-04-19 15:49:31 -0300

    add: inserindo imagem

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,5 +1,5 @@
 import styles from './Resume.module.css'
-import teste from '/assets/city.jpg'
+import teste from 'assets/city.jpg'
 
 export default function Resume(){
     return(
@@ -10,12 +10,14 @@ export default function Resume(){
         <p className={styles.paragraph}>Nier Automata é um jogo de RPG de ação com elementos "hack and slash" desenvolvido pela Platinum Games e publicado pela Square Enix em 2017. É o segundo jogo da série NieR, uma sequência indireta do jogo NieR Replicant. NieR Automata acontece no ano 11.945 durante a 14ª Guerra das Máquinas</p>
         </figure>
 
+        <img  className={styles.imageBackground} src={teste}/>
+
+        
         <h2>Enredo</h2>
         <figure>
         <p className={styles.paragraph}>Nier Automata é um jogo de RPG de ação com elementos "hack and slash" desenvolvido pela Platinum Games e publicado pela Square Enix em 2017. É o segundo jogo da série NieR, uma sequência indireta do jogo NieR Replicant. NieR Automata acontece no ano 11.945 durante a 14ª Guerra das Máquinas</p>
         </figure>
 
-        <img src={teste}/>
 
         </div>
     )
