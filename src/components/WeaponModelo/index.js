39a41fc8593fa6543bbf3a8a22f2201e63b4fdf4
DavidGabrielBJorge <david.jorge@estudante.iftm.edu.br>
2023-05-01 09:33:39 -0300

    fix: ao clicar em uma arma vai redirecionar o usuario para o topo da página

--- src/components/WeaponModelo/index.js
+++ src/components/WeaponModelo/index.js
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
 import styles from './WeaponModelo.module.css'
 
-export default function weaponModelo({children, fotoCapa, titulo}){
+export default function WeaponModelo({children, fotoCapa, titulo}){
+
+    useEffect(() => {
+        window.scrollTo(0, 0); // define a posição do scroll para o topo
+      }, []);
+
     return(
         <article className={styles.weaponModeloContainer}>
             <div className={styles.fotoCapa} style={{background:`url(${fotoCapa})`}}>
